efdd8e142241e619a2097b52598dfc2f2341d8bd
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-11-22 22:37:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 //your JS code here. If required.
-function functionName() {
-    alert("functionName");
+function functionName(){
+	alert("functionName");
 }
-
-// Calling the function
 functionName();
-
-function AccioJob() {
-    alert(arguments.callee.name);
-}
-
-// Calling the function
-AccioJob();
